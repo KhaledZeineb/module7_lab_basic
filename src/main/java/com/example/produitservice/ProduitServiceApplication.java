6ce2df23cdea66e54404a28a03fdc1d4198d105c
KhaledZeineb <khaledzeineb81@gmail.com>
2025-05-05 18:40:51 +0100

    product-service

---
+++ src/main/java/com/example/produitservice/ProduitServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.produitservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ProduitServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProduitServiceApplication.class, args);
+    }
+
+}
